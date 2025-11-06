46a60f44e4444ff8b9f3d5e9a949a3443f8ec7fc
Pedro Jano <pedrino022@gmail.com>
2025-11-06 11:44:35 -0300

    Project aimed at the original version

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,44 +1,37 @@
-// export const api = process.env.REACT_APP_API || "/api";
-// export const uploads = "http://localhost:5000/uploads";
-
-const isDevelopment = process.env.NODE_ENV === 'development';
-
-export const api = isDevelopment 
-    ? "http://localhost:5000/api"  
-    : "/api";                      
-
-export const uploads = isDevelopment 
-    ? "http://localhost:5000/uploads" 
-    : "/uploads";
-
+export const api = "http://localhost:5000/api";
+export const uploads = "http://localhost:5000/uploads";
 
 export const requestConfig = (method, data, token = null, image = null) => {
-  let config;
+ 
+    let config 
 
-  if (image) {
-    config = {
-      method,
-      body: data,
-      headers: {},
-    };
-  } else if (method === "DELETE" || data === null) {
-    config = {
-      method,
-      headers: {},
-    };
-  } else {
-    config = {
-      method,
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-  }
+    if(image) {
+        config = {
+            method,
+            body: data,
+            headers: {}
+        };
+    } else if(method === "DELETE" || data === null) {
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+        config = {
+            method,
+            headers: {}
+        };
+    } else {
+        config = {
+            method, 
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json",
+                
+            }
+        };
 
-  return config;
-};
+    }
+
+    if(token) {
+        config.headers.Authorization =  `Bearer ${token}`;
+    }
+
+    return config;
+}
